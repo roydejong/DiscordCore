06f5ce85a77cfc16c06070656ee7d7f12b94583b
FizzyApple12 <davideroeder@gmail.com>
2021-07-26 23:49:42 -0400

    Bumped versions and added FizzyUtils integration

--- DiscordCore/Plugin.cs
+++ DiscordCore/Plugin.cs
@@ -1,17 +1,23 @@
-﻿using IPA;
+﻿using FizzyUtils;
+using IPA;
 
 namespace DiscordCore
 {
     [Plugin(RuntimeOptions.SingleStartInit)]
     public class Plugin
     {
+        internal const string Name = "DiscordCore";
         internal static IPA.Logging.Logger log;
+
+        UsageTracker.UsageTrackerUser usageTrackerUser;
 
         [Init]
         public void Init(IPA.Logging.Logger log)
         {
             Plugin.log = log;
             DiscordManager manager = DiscordManager.instance;
+
+            usageTrackerUser = Utils.usageTracker.AddUser(Name);
         }
 
         [OnEnable, OnDisable]
