af4254cdd89a8b069669affb7beeb6619177f969
Roy de Jong <email@roydejong.net>
2021-09-17 09:25:34 +0200

    remove fizzyutils websocket stuff

--- DiscordCore/Plugin.cs
+++ DiscordCore/Plugin.cs
@@ -1,5 +1,4 @@
-﻿using FizzyUtils;
-using IPA;
+﻿using IPA;
 
 namespace DiscordCore
 {
@@ -9,18 +8,11 @@ namespace DiscordCore
         internal const string Name = "DiscordCore";
         internal static IPA.Logging.Logger log;
 
-        UsageTracker.UsageTrackerUser usageTrackerUser;
-
         [Init]
         public void Init(IPA.Logging.Logger log)
         {
             Plugin.log = log;
             DiscordManager manager = DiscordManager.instance;
-
-            usageTrackerUser = Utils.usageTracker.AddUser(Name);
         }
-
-        [OnEnable, OnDisable]
-        public void OnState() { /* On State */ }
     }
 }
